218eb216aa204c9a6c82b42e9e7995158ce482f3
Kyle <kb0531@cs.washington.edu>
2025-08-10 21:19:36 +0900

    created products routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,39 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  prefix,
+} from "@react-router/dev/routes";
 
-export default [index("common/pages/home-page.tsx")] satisfies RouteConfig;
+export default [
+  index("common/pages/home-page.tsx"),
+  ...prefix("products", [
+    index("features/products/pages/products-page.tsx"),
+    ...prefix("leaderboards", [
+      index("features/products/pages/leaderboard-page.tsx"),
+      route(
+        "/yearly/:year",
+        "features/products/pages/yearly-leaderboard-page.tsx"
+      ),
+      route(
+        "/monthly/:year/:month",
+        "features/products/pages/monthly-leaderboard-page.tsx"
+      ),
+      route(
+        "/daily/:year/:month/:day",
+        "features/products/pages/daily-leaderboard-page.tsx"
+      ),
+      route(
+        "/weekly/:year/:week",
+        "features/products/pages/weekly-leaderboard-page.tsx"
+      ),
+    ]),
+    ...prefix("categories", [
+      index("features/products/pages/categories-page.tsx"),
+      route("/:category", "features/products/pages/category-page.tsx"),
+    ]),
+    route("/search", "common/pages/search-page.tsx"),
+    route("/submit", "common/pages/submit-page.tsx"),
+    route("/promote", "common/pages/promote-page.tsx"),
+  ]),
+] satisfies RouteConfig;
